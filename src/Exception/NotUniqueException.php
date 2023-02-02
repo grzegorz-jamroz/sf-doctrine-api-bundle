49d59fc8ad036046c7235c370db3af770cefb7cd
Grzechu <grzegorz.jamrooz@gmail.com>
2023-02-02 18:50:55 +0100

    extends NotUniqueException with Ifrost\ApiFoundation\Exception\NotUniqueException

--- src/Exception/NotUniqueException.php
+++ src/Exception/NotUniqueException.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Ifrost\DoctrineApiBundle\Exception;
 
-class NotUniqueException extends \Exception
+use Ifrost\ApiFoundation\Exception\NotUniqueException as ApiNotUniqueException;
+
+class NotUniqueException extends ApiNotUniqueException
 {
 }
